0f7b53d9d11951375ace19c3a19c14598e22cf0a
elfas <kibetelfas17@gmail.com>
2025-03-17 22:59:44 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+// Part 1: writeCards
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  }
+  return messages;
+}
+
+// Part 2: countDown
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+// Example calls to test:
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+countDown(10);
